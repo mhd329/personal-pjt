3c4832733b07ec64ff600ef594e0c6f7bb17e4f3
mhd329 <mhd329@gmail.com>
2023-05-12 22:25:34 +0900

    2023 5 12 react-todo

---
+++ src/Components/Router.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "../Pages/Login/Login";
+import Todos from "../Pages/Todos/Todos";
+
+export default function Router() {
+  return (
+    
+  )
+}
+
+<BrowserRouter>
+  <Routes>
+    <Route />
+  </Routes>
+</BrowserRouter>
