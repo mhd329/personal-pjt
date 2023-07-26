8e3bdb3951dec0715fb5624d99b56d2a1388d291
mhd329 <mhd329@gmail.com>
2023-07-26 18:03:05 +0900

    travis yml Added

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,11 @@ env.config();
 const mysql = require("mysql2");
 const pool = mysql.createPool({
     // docker 사용 시 호스트는 docker에서 설정한 컨테이너 이름으로 해야되는 것 같다.
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
 });
 
 exports.pool = pool;
