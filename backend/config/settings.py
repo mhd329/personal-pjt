7cccde59542329bdc8b053712b0f6feabd8bb466
mhd329 <mhd329@gmail.com>
2023-08-01 22:45:21 +0900

    2023 8 1 deploy via travis

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -42,12 +42,12 @@ if DEBUG:
     ]
 else:
     ALLOWED_HOSTS = [
-        "*",
+        "backend",
     ]
-    # CORS_ALLOW_CREDENTIALS = True
-    # CORS_ORIGIN_WHITELIST = [
-    #     "http://docker-todo-project-env.eba-5cpsn9ph.ap-northeast-2.elasticbeanstalk.com",
-    # ]
+    CORS_ALLOW_CREDENTIALS = True
+    CORS_ORIGIN_WHITELIST = [
+        "http://frontend",
+    ]
 
 
 # Application definition
