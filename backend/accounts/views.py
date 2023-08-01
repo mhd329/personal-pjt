c0186360d65384ce5c7e4b7c7833a16c457688a2
mhd329 <mhd329@gmail.com>
2023-08-02 01:04:37 +0900

    2023 8 2 deploy via travis

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -288,14 +288,14 @@ class LoginView(APIView):  # 로그인
                         "access",
                         access,
                         secure=True,
-                        samesite="lax",
+                        samesite="none",
                     )
                     res.set_cookie(
                         "refresh",
                         refresh,
                         httponly=True,
                         secure=True,
-                        samesite="lax",
+                        samesite="none",
                     )
                     return res
                 else:
