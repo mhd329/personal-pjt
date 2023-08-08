b716d573605fa146a36e10caac0319101b3e6f4e
mhd329 <mhd329@gmail.com>
2023-08-09 05:10:44 +0900

    2023 8 9 docker-fullstack-app deploy on aws via travisCI

--- websocket/redisClient.js
+++ websocket/redisClient.js
@@ -4,7 +4,7 @@ env.config();
 
 // 레디스 클라이언트 연결
 const redisClient = createClient({
-    url: process.env.DEBUG === "true" ? "redis://localhost:6379" : process.env.REDIS_HOST
+    url: process.env.DEBUG === "true" ? "redis://localhost:6379" : `redis://${process.env.REDIS_HOST}`
 });
 
 export default redisClient;
