8b74aac310a73d1cc3f5070593d61ddc97956d86
mhd329 <mhd329@gmail.com>
2023-08-16 17:06:50 +0900

    2023 8 16 modify: todoapp nginx default.conf

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -162,10 +162,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "/media/"
+# MEDIA_URL = "/media/"
+MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 # Default primary key field type
