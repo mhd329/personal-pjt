f74db335d86a2fd70ebf822b8c991ffa05761d32
mhd329 <mhd329@gmail.com>
2023-09-27 21:51:39 +0900

    2023 09 27 feature/crawler multiprocessing

--- db/sqls/initialize.sql
+++ db/sqls/initialize.sql
@@ -1,5 +1,13 @@
 DROP DATABASE IF EXISTS crawling;
-
 CREATE DATABASE crawling;
-
 USE crawling;
+CREATE TABLE computer_specs (
+    id INTEGER AUTO_INCREMENT,
+    main_board VARCHAR,
+    CPU VARCHAR,
+    RAM VARCHAR,
+    VGA VARCHAR,
+    storage VARCHAR,
+    power_supply VARCHAR,,
+    PRIMARY KEY (id)
+);
