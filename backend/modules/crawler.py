51bbc770e5dcbd8a4307dce464b3382afb1c0dc5
mhd329 <mhd329@gmail.com>
2023-10-08 23:10:58 +0900

    2023 10 08 document trials and errors

--- backend/modules/crawler.py
+++ backend/modules/crawler.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from .driver import CustomChromeDriverManager
 
 
