f8e1ad932049616d21c1f9450075326d8dce048c
mhd329 <mhd329@gmail.com>
2024-02-08 19:48:28 +0900

    2024 2 8 modified main.py.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
+import sys
 import asyncio
 import subprocess
-from datetime import datetime, timezone
 from dotenv import load_dotenv
 
 
@@ -12,13 +12,13 @@ load_dotenv()
 
 
 token_bot = os.getenv("TOKEN_BOT")
-token_server = os.getenv("TOKEN_SERVER_DEBUG")
-
-
-bot = Settings(server_id=token_server)
+token_server = os.getenv("TOKEN_SERVER")
 
 
 if __name__ == "__main__":
+    if "debugpy" in sys.modules:
+        token_server = os.getenv("TOKEN_SERVER_DEBUG")
+    bot = Settings(server_id=token_server)
     asyncio.run(bot.add_cog(Commands(bot)))
     @bot.tree.command(name = "종료", description = "서버 컴퓨터 종료.")
     async def terminate_server(interaction):
