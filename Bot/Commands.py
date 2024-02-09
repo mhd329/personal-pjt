630dc831cdf2e55dcbeafb350eff6fd0b1d8f6cb
mhd <mhd329@gmail.com>
2024-02-10 06:20:55 +0900

    modified Commands.py.

--- Bot/Commands.py
+++ Bot/Commands.py
@@ -2,7 +2,7 @@ import os
 import random
 import asyncio
 import subprocess
-from discord import Embed, Color
+from discord import Embed, Color, Message
 from discord.ext import commands
 from Log.Settings import logger, logger_detail
 
@@ -35,7 +35,7 @@ class Commands(commands.Cog):
                     except Exception as error:
                         logger.error("ERROR : log_detail_palserver.log 참조")
                         logger_detail.error(error)
-            ebd = Embed(title=f"\n:eyes: 서버 상태\n\t{msg}\n\n", description=f"\n:bulb: 서버 실행시간\n\t{result}\n\n:globe_with_meridians: 서버 아이피\n\t{server_ip}:8211\n", color=state_color)
+            ebd = Embed(title=f"\n:eyes: 서버 상태\n{msg}", description=f"\n\n:bulb: 서버 실행시간\n\t{result}\n\n:globe_with_meridians: 서버 아이피\n\t{server_ip}:8211\n", color=state_color)
             ebd.set_thumbnail(url=image_url)
             ebd.set_author(name=self.bot.user.display_name, icon_url = self.bot.user.display_avatar)
             return ebd
@@ -63,7 +63,7 @@ class Commands(commands.Cog):
 
     @commands.command(aliases=["명령", "명령어"])
     async def find_command(self, ctx):
-        ebd = Embed(title="명령어 모음", description="서버 원격 조종 명령어 모음 안내입니다.\n자세한 사항은 [여기](https://github.com/mhd329/palserver-remote-control)를 참조하세요.\n```md\n[!!상태][서버 상태 확인하기.]\n\n[!!열기][서버 열기.]\n\n[!!닫기][서버 닫기.]\n\n[!!업데이트][서버 업데이트 하기.]\n```")
+        ebd = Embed(title="명령어 모음", description="서버 원격 조종 명령어 모음 안내입니다.\n자세한 사항은 [여기](https://github.com/mhd329/palserver-remote-control)를 참조하세요.\n```md\n 1. [!!핑][서버 핑 확인하기.]\n\n 2. [!!인사][봇과 인사하기.]\n\n 3. [!!명령][봇 명령어 확인.]\n\n 4. [!!상태][서버 상태 확인.]\n\n 5. [!!열기][서버 열기.]\n\n 6. [!!닫기][서버 닫기.]\n\n 7. [!!업데이트][서버 업데이트.]\n```")
         ebd.set_thumbnail(url="https://cdn.discordapp.com/attachments/995736483854036994/1205592066768379944/cogs.png?ex=65d8ee1b&is=65c6791b&hm=ecb652eceda7a55fca809a5641e64ccdaad6a95ee90040f4c2c5e016972bef33&")
         ebd.set_author(name=self.bot.user.display_name, icon_url = self.bot.user.display_avatar)
         ebd.set_footer(text = f"{ctx.message.author.display_name}", icon_url = ctx.message.author.display_avatar)
@@ -87,7 +87,7 @@ class Commands(commands.Cog):
         ebd.set_thumbnail(url="https://cdn.discordapp.com/attachments/995736483854036994/1205592106110820383/android.png?ex=65d8ee24&is=65c67924&hm=1106bc390dc587d8b5a328d23dd03a515fda2a178761aa62ca7f3914edc7ce6c&")
         ebd.set_author(name=self.bot.user.display_name, icon_url = self.bot.user.display_avatar)
         ebd.set_footer(text = f"{ctx.message.author.display_name}", icon_url = ctx.message.author.display_avatar)
-        await ctx.send(embed = ebd)
+        await msg.edit(embed = ebd)
         del ebd
 
     @commands.command(name="상태")
@@ -102,12 +102,13 @@ class Commands(commands.Cog):
 
     @commands.command(name="열기")
     async def open_server(self, ctx):
+        msg = await ctx.send("서버를 시작합니다. 잠시만 기다려주세요...")
         try:
             await self.run_command("./run_palserver.sh")
             try:
                 ebd = await asyncio.to_thread(self.check_server)
                 ebd.set_footer(text = f"{ctx.message.author.display_name}", icon_url = ctx.message.author.display_avatar)
-                await ctx.send(embed = ebd)
+                await msg.edit(embed = ebd)
                 del ebd
             except Exception as error:
                 await ctx.send(error)
@@ -122,12 +123,13 @@ class Commands(commands.Cog):
 
     @commands.command(name="닫기")
     async def close_server(self, ctx):
+        msg = await ctx.send("서버를 종료합니다. 잠시만 기다려주세요...")
         try:
             await self.run_command("./close_palserver.sh")
             try:
                 ebd = await asyncio.to_thread(self.check_server)
                 ebd.set_footer(text = f"{ctx.message.author.display_name}", icon_url = ctx.message.author.display_avatar)
-                await ctx.send(embed = ebd)
+                await msg.edit(embed = ebd)
                 del ebd
             except Exception as error:
                 await ctx.send(error)
@@ -140,13 +142,14 @@ class Commands(commands.Cog):
 
     @commands.command(name="업데이트")
     async def update_server(self, ctx):
+        msg = await ctx.send("업데이트 중입니다. 잠시만 기다려주세요...")
         try:
             await self.run_command("./update_palserver.sh")
             ebd = Embed(title="업데이트", description="https://store.steampowered.com/news/app/1623730")
             ebd.set_thumbnail(url="https://cdn.discordapp.com/attachments/995736483854036994/1205592106110820383/android.png?ex=65d8ee24&is=65c67924&hm=1106bc390dc587d8b5a328d23dd03a515fda2a178761aa62ca7f3914edc7ce6c&")
             ebd.set_author(name=self.bot.user.display_name, icon_url = self.bot.user.display_avatar)
             ebd.set_footer(text = f"{ctx.message.author.display_name}", icon_url = ctx.message.author.display_avatar)
-            await ctx.send(embed = ebd)
+            await msg.edit(embed = ebd)
         except FileNotFoundError:
             logger.info("update_palserver.sh 파일 없음.")
             await ctx.send(f"해당 위치({os.getcwd()})에 업데이트 스크립트가 존재하지 않습니다.")
